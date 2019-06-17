f1497604398514a0521b8b3ff07de2ab3ae526c0
Karla Gallegos <kgalleg618@gmail.com>
2019-06-17 12:59:10 -0500

    Practicing ES6 Modules Exercise

---
+++ add.js
@@ -0,0 +1,13 @@
+// 1) Make main/add/subtract js modules
+// 2) Create a private variable of 10 in both add and subtract
+// 3) Create add and subtract functions in their respective modules that act on a number passed into them and the private var
+// 4) Import both modules into main.js
+// 5) execute both function in main.js
+
+let addVariable = 10;
+function addingNumber(number) {
+    console.log(addVariable + number)
+}
+
+export {addingNumber}
+
---
+++ main.js
@@ -0,0 +1,11 @@
+// 1) Make main/add/subtract js modules
+// 2) Create a private variable of 10 in both add and subtract
+// 3) Create add and subtract functions in their respective modules that act on a number passed into them and the private var
+// 4) Import both modules into main.js
+// 5) execute both function in main.js
+
+import{addingNumber} from "./add.js"
+import{subtractNumber} from "./subtract.js"
+
+addingNumber(2);
+subtractNumber(3);
